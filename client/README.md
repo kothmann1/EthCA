d8b383f30b55ca4ef870638bcbc3bec4cb80e872
Katie Martins <kem5en@virginia.edu>
2022-05-10 18:36:28 -0400

    Updated client interface

--- client/README.md
+++ client/README.md
@@ -3,5 +3,7 @@
 - Gets serial number from server (in this case 0)
 - Checks if connected to blockchain before calling contract functions
 - Gets pk and domain from certificate (right now printing just the domain)
+- Use message.txt for abi or the one copied into the code
+- change IPC path to the one specified on your machine
 
 Commented out sections allow for different connection options (auto/manual)
--- client/client_interfacing.py
+++ client/client_interfacing.py
@@ -1,8 +1,8 @@
 from web3 import Web3
 #from web3.auto import Web3
-from web3 import providers
+#from web3 import providers
 
-w3 = Web3(Web3.IPCProvider(providers.ipc.get_default_ipc_path()))
+w3 = Web3(Web3.IPCProvider('\\.\pipe\geth-data1.ipc'))
 #w3 = Web3()
 
 #file = open("C:\Users\katma\Desktop\CS6501_NSP\project\client\message.txt", "r")
@@ -148,9 +148,6 @@ abi = [
 # block to verify (serial_num)
 serial_num = 0
 contract_instance = w3.eth.contract(address=address, abi=abi)
-pk, domain, issued, exp = contract_instance.functions.readCertificate(serial_num).call()
-print('Domain: ', domain)
-
 
 if w3.isConnected():
     # what function to call?
